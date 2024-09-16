32bf8f5d60f6f416140ae641259060b3b08ec085
Anas zahoor <ngsanas04@gmail.com>
2024-09-16 19:22:33 +0500

    js function

---
+++ v.js
@@ -0,0 +1,14 @@
+var input = document.getElementById("name");
+var fatherInput = document.getElementById("father");
+var numberinput = document.getElementById("number");
+function print() {
+  alert("resgistration done");
+  console.log(
+    "your nanme:",
+    input.value,
+    "father name:",
+    fatherInput.value,
+    "age:",
+    numberinput.aos
+  );
+}
